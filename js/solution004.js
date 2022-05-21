5feb679122781087fe229c38498bb385f1c710d6
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2022-05-21 16:23:29 -0700

    solution 004

---
+++ js/solution004.js
@@ -0,0 +1,34 @@
+// The span function is a good one to know. It accepts an array and a predicate function and returns two arrays. The first array contains all the elements of the argument array up to the item that caused the first failure of the predicate. The second returned array contains the rest of the original array. The original array is not modified.
+
+// For example,
+
+
+// function isEven (x) {
+//   return Math.abs(x) % 2 === 0;
+// }
+
+// var arr = [2,4,6,1,8,10];
+
+// // This is true
+// span(arr, isEven) === [[2,4,6],[1,8,10]]
+// Your task is to...wait for it... write your own span function!!!
+
+// Hint/Challenge: If you have completed the takeWhile function and the dropWhile function, then you can solve this problem in one line!
+
+
+
+function span(arr, predicate) {
+    //   P arry of things, predict call back function
+    //   R two arrys, first bireng elelments until the predicate returns false, second bieng
+    //   E
+    //   P
+      const pass = [];
+      
+      for (let i = 0; i < arr.length; i++){
+        if (predicate(arr[i])) pass.push(arr[i])
+        else return [pass, arr.slice(i)]
+      }
+      return [pass,[]]
+    }
+    
+    
