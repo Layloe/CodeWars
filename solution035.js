d2e98612f9470ebe8b03cca5442ac077ce176085
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2022-09-02 06:21:41 -0700

    String Anki Hunt

---
+++ solution035.js
@@ -0,0 +1,22 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+// // STRINGSFUNDAMENTALS
+// Parameters: Remove first and last characters of a string, string wont be less then two charactors
+// Return: a string that has removed first and last charactors
+// Examples: 
+// assert.strictEqual(removeChar('eloquent'), 'loquen');
+//     assert.strictEqual(removeChar('country'), 'ountr');
+//     assert.strictEqual(removeChar('person'), 'erso');
+//     assert.strictEqual(removeChar('place'), 'lac');
+//     assert.strictEqual(removeChar('ooopsss'), 'oopss');
+// Psudocode: Slice at 1 and -1
+
+
+function removeChar(str){
+  return str.slice(1, -1)
+   
+   };
+
+   removeChar('meow')
+
+   
