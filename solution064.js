1f3443839d0b32f75c4fed10ffaf15aec35fbab2
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2022-12-08 18:20:02 -0800

    String and array methods

---
+++ solution064.js
@@ -0,0 +1,36 @@
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+//Parameters: A string which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+//Return: A string 
+
+//Exapmle:
+    // Test.assertSimilar(getNumberFromString("1"), 1);
+    // Test.assertSimilar(getNumberFromString("123"), 123);
+    // Test.assertSimilar(getNumberFromString("this is number: 7"), 7);
+
+
+
+//Psuedocode: USe methods split('').filter(s =>) , includes(s). join('')
+
+// const assert = require('assert')
+
+
+function getNumberFromString(s) {
+//    let nums = "1234567890"
+    
+    return (s.split('').filter(s => '1234567890' .includes(s)).join('')) 
+
+  }
+
+  getNumberFromString('this is number: 7')
+
+  //! Tried fixing assert ¯\_ (ツ)_/¯
+
+//   assert.strictEqual(getNumberFromString("1"), 1);
+//   assert.strictEqual(getNumberFromString("123"), 123);
+//   assert.strictEqual(getNumberFromString("this is number: 7"), 7);
