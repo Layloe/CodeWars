b9daa848fa1dcd4ecb3e9991196979ee0a548efb
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2022-12-21 17:54:41 -0800

    Array methods

---
+++ solution074.js
@@ -0,0 +1,30 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+//Paramters: nums
+
+//Return: Convert numbers to reversed array of digits
+
+//Example:
+//     assert.deepEqual(digitize(35231),[1,3,2,5,3]);
+//     assert.deepEqual(digitize(0),[0]);
+
+//Psuedocode: Use methods toString().split('').reverse().map()
+
+
+const assert = require('assert')
+
+
+function digitize(n) {
+    return n.toString().split('').reverse().map(Number);
+  }
+
+
+
+console.log(digitize(35231))
+    assert.deepEqual(digitize(35231),[1,3,2,5,3]);
+    assert.deepEqual(digitize(0),[0]);
