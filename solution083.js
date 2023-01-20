468a013ad68cd7708ab71be8842d1e0a4396ef8e
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-01-19 21:10:44 -0800

    Date methods

---
+++ solution083.js
@@ -0,0 +1,67 @@
+
+
+/**
+ * This is a series of exercises to practice composing functions.
+ * The idea is, rather than doing everything in one big function,
+ * we can break out the different behaviors we need into separate functions.
+ */
+
+/**
+ * Task 1:
+ * Write a function that gets the day of the week from a date string
+ */
+
+//Parameters:
+
+//Example:
+
+//Retrun:
+
+//Psuedocode:
+
+
+// const assert = require("assert");
+
+function getDayOfWeek(dateString) {
+
+
+  
+       const weekday= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+       const today = new Date()
+        
+
+       return dateString[today.getUTCDay(weekday)]
+
+//      weekday[0] { date: "2023-01-15T03:00:00Z" } = "sunday",
+//       { date: "2023-01-16T03:00:00Z" } = "monday",
+// weekday[2]="tuesday",
+// weekday[3]="wednesday",
+// weekday[4]="thursday",
+// weekday[5]="friday",
+// weekday[6]="saturday"
+//     ]
+
+// var n = weekday[d.getDay()];
+
+
+//       { date: "2023-01-15T03:00:00Z" },
+//       { date: "2023-01-16T03:00:00Z" },
+//       { date: "2023-01-17T03:00:00Z" },
+//       { date: "2023-01-18T03:00:00Z" },
+//       { date: "2023-01-19T03:00:00Z" },
+//       { date: "2023-01-20T03:00:00Z" },
+//       { date: "2023-01-21T03:00:00Z" },
+// ]
+// return dateString.getUTCDay()     
+    // hint: you can create a Date with "new Date(dateString)"
+    // hint: you can use the `getUTCDay()` method to get a number corresponding to the week day,
+    // for example 0 = sunday, 1 = monday, etc
+    // TODO
+  }
+
+  console.log(getDayOfWeek("2023-01-15T03:00:00Z"))
+
+  
+//   assert.strictEqual(getDayOfWeek("2023-01-18T03:00:00Z"), "wednesday");
+//   assert.strictEqual(getDayOfWeek("2023-01-17T03:00:00Z"), "tuesday");
+//   assert.strictEqual(getDayOfWeek("2023-01-19T13:00:00Z"), "thursday");
