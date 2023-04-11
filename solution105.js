e958e3c12078e8663b29b69f40c2faba7f74095a
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-04-10 19:13:56 -0700

    array practice

---
+++ solution105.js
@@ -0,0 +1,22 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+//Parameters: Array of elements
+
+//Return: String with comma delimited elements of the array in th same order
+
+//Example: See above
+
+//Psuedocode: USe method join
+
+function printArray(array){
+     return  array.join(',')
+
+  }
+
+  console.log(printArray(["h","o","l","a"]))
