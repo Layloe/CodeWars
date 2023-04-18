c922e595dd610d3fae0ef71ca012ab6613169782
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-04-17 20:12:02 -0700

    array challenge

---
+++ solution106.js
@@ -0,0 +1,20 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// Instructions unclear for a codewars challenge; websites.push("codewars")
+
+function addArr(param) {
+    let websites = [param] 
+
+    // console.log(websites)
+
+    return websites
+
+    
+}
+
+console.log(addArr("codewars"))
+
+
