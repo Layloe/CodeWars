4baabe5f53ef1fa8a85481ca27db133856365269
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-04-29 09:34:39 -0700

    Leveled up in array challenges

---
+++ solution113.js
@@ -0,0 +1,53 @@
+// Order People by age Using Arrow Function
+// Sort and Order people by their age using Arrow Functions
+// Task
+// Your task is to order a list containg people objects by age using the new Javascript Arrow Functions
+// Input
+// Input will be a valid array with People objects containing an Age and Name
+// Output
+// Output will be a valid sorted array with People objects sorted by Age in ascending order
+// (cond ? a : b)
+
+//Parameters: list containg people objects by age using the new Javascript Arrow Functions
+
+//Return: a valid sorted array with People objects sorted by Age in ascending order
+
+//Example:
+// ([ { age: 83, name: 'joel' },
+// { age: 46, name: 'roger' },
+// { age: 99, name: 'vinny' },
+// { age: 26, name: 'don' },
+// { age: 74, name: 'brendan' } ]), [{age:26,name:"don"},{age:46,name:"roger"},{age:74,name:"brendan"},{age:83,name:"joel"},{age:99,name:"vinny"}], 'The array is still empty')
+//   }); 
+
+//Psuedocode: Simplify function by using arrow function and using sort method
+
+
+
+// function OrderPeople(people) {
+//     let test = people.sort((a, b) => a.age - b.age)
+
+//     let property = people.map((value) => ({
+//         age: [value.age]
+//     }))
+
+//     return test
+    
+// }
+
+
+
+// let OrderPeople = function(people){
+//     return people.sort( => ); //complete this function
+//   }
+
+  let OrderPeople = (people) => people.sort((a, b) => a.age - b.age)
+
+
+
+
+  console.log(OrderPeople([ { age: 83, name: 'joel' },
+  { age: 46, name: 'roger' },
+  { age: 99, name: 'vinny' },
+  { age: 26, name: 'don' },
+  { age: 74, name: 'brendan' } ]))
