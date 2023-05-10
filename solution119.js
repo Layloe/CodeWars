c49484fd8d2c24deed72864c1485f67fcfc1b18d
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-05-10 06:28:35 -0700

    arr filter and modulus operator challenge

--- solution119.js
+++ solution119.js
@@ -10,7 +10,7 @@
 // assert.deepEqual( noOdds( [0,1] ), [0] )
 // assert.deepEqual( noOdds( [0,1,2,3] ), [0,2] )
 
-// Psuedocode: use filter method to compare if the value can be divided by 2
+// Psuedocode: use filter method to compare if the value can be divided by 2 **use modulus insead of divide
 
 //? Kept forgeting to use % instead of /
 
@@ -18,9 +18,9 @@ const assert = require('assert')
 
 
 function noOdds( values ){
-    let test = []
+    // let test = []
     return values.filter((value) => value % 2 === 0  )
-    console.log(test)
+    // console.log(test)
   }
 
   console.log(noOdds([0,1,2,3]))
