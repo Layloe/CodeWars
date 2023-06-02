7e086b8622cdd9a7e0278e764d6f14479bb20aaa
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-06-02 10:20:09 -0700

    First attemt at using “Arrange, Act, Assert” software testing methodology

--- solution123.js
+++ solution123.js
@@ -21,15 +21,34 @@
 
 const assert = require('assert')
 
-function duplicates(arr) {
-    // let dupes = arr.map(value => value)
-    let test = arr.filter((value, index) => arr.indexOf(value) !== index)
+const duplicates = arr => [...new Set(arr.filter((x, i) => arr.indexOf(x) != i))] //? Solution from codewars
 
-    return test
-  }
+// function duplicates(arr) {
+  
+    // let test = arr.filter((value, index) => arr.indexOf(value) !== index)
+    // console.log(test)
+    // return test
+//   }
 
   console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]))
 
 assert.deepEqual(duplicates([1, 2, 3, 4, '3']), [], "arr = [1, 2, 3, 4, '3']");
 assert.deepEqual(duplicates(['1', 2, 3, 3, '2', 1]), [3], "arr = ['1', 2, 3, 3, '2', 1]");
 assert.deepEqual(duplicates(['zut', 'alors', 1, 2, 4, 4, 3, 3, '1', 5, 3, 'zut']), [4, 3, 'zut'], "arr = ['zut', 'alors', 1, 2, 4, 4, 3, 3, '1', 5, 3, 'zut']");
+
+// Eh!,Eh!,Eh!
+// Arrange: The function seems to be missing or skipping a dupe and I'm not sure why yet, but I think it had to do with either them being multiables of the same number or the same number right next to each other in the arr. I'm going to try to use console.log to get more information, then use google to hopefully find a bread crumb.
+
+//It's getting this err -->
+//? code: 'ERR_ASSERTION',
+//? actual: [ 4, 3, 3, 'zut' ],
+//? expected: [ 4, 3, 'zut' ],
+//? operator: 'deepEqual'
+
+// Act:I tried using a visualisation tool(still need to practice using this):https://jsviz.klve.nl/#?code=GYVwdgxgLglg9mABAExABwDYwgQygUwGcAKHAJzIEpEBvAKEUcQHpnEN8oV0jEBeROTIA6ALY40xAG44MIfPwB8iGXPyUGTDlwKEuAocOAwMBMsWmz5AGkQwwyfAA9qfZYfuOnAeWCW11ACEfAKezhpMiBAIhHAcwhhwAObEulARTGScIGRIaZoAvnSa0WCx8YkpqJjYeETEANoA5ABeIFBNtk2ycGSEnYgAjLYATLYALBO2AMwzXYMDAKxziK3tTQC6lJRAA and still couldn't figure out what is going wrong. Going to use GPT to help me find out what's going wrong.
+
+// Assert: I was on the right path as far as problem solving goes, but the method I was using, "indexOf()," stops once it finds the value/item. So if the item occurs more than once in the arr it won't 'include' it in the filtered items. The solution was to use the includes() method inside my filter. This way, if there are multiple dupes, it will filter them.
+
+//! Turns out there are still more problems but I've ran out time, I went over time by almost double oof. I'm sure this will get easier the more I do it.
+
+//! First attemt at using “Arrange, Act, Assert” software testing methodology. I mainly just spelt out my thought process. Will refine as time goes
