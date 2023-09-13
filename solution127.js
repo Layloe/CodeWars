adbccdd996cd491f7855f0c308ff870e6c5032bd
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-09-13 07:17:52 -0700

    coding challenge arr methods

--- solution127.js
+++ solution127.js
@@ -17,13 +17,22 @@
 
 //Psuedocode: add the arr using reduce method then use ifelse to filter our the negative nums
 
-
+const assert = require('assert')
 
 
 function positiveSum(arr) {
+    // const negativeNums = negativeNums ? negativeNums <=
 
-    return arr.reduce((sum,current) => sum + current )
+
+
+    return arr.reduce((sum,current) => sum + (current > 0 ? current : 0), 0 )
   
 }
 
 console.log(positiveSum([1,-2,3,4,5]))
+
+assert.strictEqual(positiveSum([1,2,3,4,5]),15);
+assert.strictEqual(positiveSum([1,-2,3,4,5]),13);
+assert.strictEqual(positiveSum([]),0);
+assert.strictEqual(positiveSum([-1,-2,-3,-4,-5]),0);
+assert.strictEqual(positiveSum([-1,2,3,4,-5]),9);
