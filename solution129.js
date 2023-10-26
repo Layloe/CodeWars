7e1cef266005d8a4ea89f4e40b554130837dddbf
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-10-26 07:05:39 -0700

    needle at position

---
+++ solution129.js
@@ -0,0 +1,29 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+// Note: In COBOL, it should return "found the needle at position 6"
+
+// Parameters: an arr with all types of data in a random order and contains a str of "needle"
+
+// Return: str "found the needle at position " and the index of the position in the str
+
+// Example:
+// Test.assertNotEquals(findNeedle(haystack_1), undefined, "Your function didn't return anything");
+// Test.assertEquals(findNeedle(haystack_1), 'found the needle at position 3')
+// Test.assertEquals(findNeedle(haystack_2), 'found the needle at position 5') 
+// Test.assertEquals(findNeedle(haystack_3), 'found the needle at position 30')
+
+// Psuedocode:
+
+
+function findNeedle(haystack) {
+    // your code here
+  }
