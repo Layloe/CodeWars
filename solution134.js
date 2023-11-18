7671038cdb72f7dc7585773c7119cb89f6f7d4e6
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2023-11-18 09:59:19 -0800

    for loop

--- solution134.js
+++ solution134.js
@@ -5,19 +5,35 @@
 
 
 
-// Parameters: an arr of nums
+// Parameters: an arr of nums one being the [value, integer]
 
-// Return:
+// Return: an arr of the value and it's multibles
 
 // Example:
-assert.sameOrderedMembers(findMultiples(5, 25), [5, 10, 15, 20, 25])
-assert.sameOrderedMembers(findMultiples(1, 2), [1, 2])
-assert.sameOrderedMembers(findMultiples(5, 7), [5])
-assert.sameOrderedMembers(findMultiples(4, 27), [4, 8, 12, 16, 20, 24])
-assert.sameOrderedMembers(findMultiples(11, 54), [11, 22, 33, 44])
+// assert.sameOrderedMembers(findMultiples(5, 25), [5, 10, 15, 20, 25])
+// assert.sameOrderedMembers(findMultiples(1, 2), [1, 2])
+// assert.sameOrderedMembers(findMultiples(5, 7), [5])
+// assert.sameOrderedMembers(findMultiples(4, 27), [4, 8, 12, 16, 20, 24])
+// assert.sameOrderedMembers(findMultiples(11, 54), [11, 22, 33, 44])
 
-// Psuedocode:
+// Psuedocode: compare and divide value by the integer using modulis, if it can't be divided return the value
 
 function findMultiples(integer, limit) {
-    //your code here
-  }
+    const testSum = []
+
+    for (let index = integer; index <= limit ; index+= integer) {
+      
+      testSum.push(index)
+      
+    }
+    // const testInt = Math.floor(limit / integer)
+    // const testLimit = limit % integer
+    //   if ( testLimit > 0 ) {
+    //   return integer
+    // } else {
+    //   return testInt
+    // }
+      return testSum
+}
+
+  console.log(findMultiples(5,25))
