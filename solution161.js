c2f1c3e117d0cf8488b2a71d1a9b4e233f9149b4
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-01-29 21:20:07 -0800

    searching lists

---
+++ solution161.js
@@ -0,0 +1,47 @@
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// For example, given the following list:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+// your function should return number 1.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+// Notes:
+
+// The format of the strings will always be Europe and JavaScript.
+// All data will always be valid and uniform as in the example above.
+
+//? Parameters: an array of objects
+
+//? Return: nums 0 for fasley and 1 for truthy
+
+//? Example:
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+
+//? Psuedocode: loop through objecs and search list then use conditionals to return a num 
+
+function countDevelopers(list) {
+
+   return list.filter(item => item.language == 'JavaScript' && item.continent == 'JavaScript').length
+   
+  }
+
+  console.log(countDevelopers([
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'JavaScript', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ]))
