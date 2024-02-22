a255a68d6d7408c099b328fd2937d899f04c65ec
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-02-21 21:16:57 -0800

    remove!

---
+++ solution199.js
@@ -0,0 +1,34 @@
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+//? Parameters: a string
+
+//? Return: a sting with exclamation mark removed form end of a string
+
+//? Example:
+// doTest("Hi!", "Hi");
+// doTest("Hi!!!", "Hi!!");
+// doTest("!Hi", "!Hi");
+
+//? Psuedocode: conditonals slice(0,-1)
+
+const assert = require('assert')
+
+// const remove = string => string.at(-1) == '!' ? string.slice(0,-1) : string
+
+function remove (string) {
+    
+    return string.at(-1) == '!' ? string.slice(0,-1) : string
+  }
+console.log(remove("Hi!!!"))
+
+  assert.strictEqual(remove("Hi!"), "Hi");
+  assert.strictEqual(remove("Hi!!!"), "Hi!!");
+  assert.strictEqual(remove("!Hi"), "!Hi");
