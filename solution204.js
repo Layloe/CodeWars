a4631c58ea22029fbac64343979379c88ef7acfe
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-02-23 07:03:31 -0800

    stringClean

---
+++ solution204.js
@@ -0,0 +1,46 @@
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+// Examples (input -> output)
+// '! !'                 -> '! !'
+// '123456789'           -> ''
+// 'This looks5 grea8t!' -> 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+//? Prameters:
+
+//? Return:
+
+//? Example:
+// assert.strictEqual(stringClean(""), "")
+// assert.strictEqual(stringClean("! !"), "! !")
+// assert.strictEqual(stringClean("123456789"), "")
+// assert.strictEqual(stringClean("(E3at m2e2!!)"), "(Eat me!!)")
+// assert.strictEqual(stringClean("Dsa32 cdsc34232 csa!!! 1I 4Am cool!"), "Dsa cdsc csa!!! I Am cool!")
+// assert.strictEqual(stringClean("A1 A1! AAA   3J4K5L@!!!"), "A A! AAA   JKL@!!!")
+// assert.strictEqual(stringClean("Adgre2321 A1sad! A2A3A4 fv3fdv3J544K5L@"), "Adgre Asad! AAA fvfdvJKL@")
+// assert.strictEqual(stringClean("Ad2dsad3ds21 A  1$$s122ad! A2A3Ae24 f44K5L@222222 "), "Addsadds A  $$sad! AAAe fKL@ ")
+// assert.strictEqual(stringClean("33333Ad2dsad3ds21 A3333  1$$s122a!d! A2!A!3Ae$24 f2##222 "), "Addsadds A  $$sa!d! A!A!Ae$ f## ")
+
+//? Psuedocode:
+//? split('')
+//? filter()
+//? !Number()
+//? join('')
+const assert = require('assert')
+
+// const stringClean = s => s.split('').filter(item => !Number(item)).join('')
+
+function stringClean(s){
+    return s.split('').filter(item => !Number(item)).join('')
+ }
+console.log(stringClean("(E3at m2e2!!)"))
+
+ assert.strictEqual(stringClean(""), "")
+ assert.strictEqual(stringClean("! !"), "! !")
+ assert.strictEqual(stringClean("123456789"), "")
+ assert.strictEqual(stringClean("(E3at m2e2!!)"), "(Eat me!!)")
+ assert.strictEqual(stringClean("Dsa32 cdsc34232 csa!!! 1I 4Am cool!"), "Dsa cdsc csa!!! I Am cool!")
+ assert.strictEqual(stringClean("A1 A1! AAA   3J4K5L@!!!"), "A A! AAA   JKL@!!!")
+ assert.strictEqual(stringClean("Adgre2321 A1sad! A2A3A4 fv3fdv3J544K5L@"), "Adgre Asad! AAA fvfdvJKL@")
+ assert.strictEqual(stringClean("Ad2dsad3ds21 A  1$$s122ad! A2A3Ae24 f44K5L@222222 "), "Addsadds A  $$sad! AAAe fKL@ ")
+ assert.strictEqual(stringClean("33333Ad2dsad3ds21 A3333  1$$s122a!d! A2!A!3Ae$24 f2##222 "), "Addsadds A  $$sa!d! A!A!Ae$ f## ")
