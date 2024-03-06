960a69a3dfba5960c16adf376e3b31e03ecfff2d
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-03-06 05:42:19 -0800

    positiveSum

--- solution224.js
+++ solution224.js
@@ -15,18 +15,22 @@
 // assert.strictEqual(positiveSum([-1,-2,-3,-4,-5]),0);
 // assert.strictEqual(positiveSum([-1,2,3,4,-5]),9);
 
-//? Psuedocode:create a varible to filer out all postive nums
+//? Psuedocode:create a varible to filer out all negitive nums    
 //? reduce()
 
-function positiveSum(arr) {
-    const positiveNums = arr.filter(item => item % 2 == 0)
-    return positiveNums.reduce((sum, curr) => curr + sum)
+const assert = require('assert')
+
+// const positiveSum = arr => arr.filter(item => item >= 0).reduce((sum, curr) => sum + curr, 0)
+
+function positiveSum(arr) {                                         
+    const positiveNums = arr.filter(item => item  >= 0)
+    return positiveNums.reduce((sum, curr) => curr + sum, 0)
   
 }
-console.log(positiveSum([1,2,3,4,5]),15)
+console.log(positiveSum([1,2,3,4,5]))
 
-// assert.strictEqual(positiveSum([1,2,3,4,5]),15);
-// assert.strictEqual(positiveSum([1,-2,3,4,5]),13);
-// assert.strictEqual(positiveSum([]),0);
-// assert.strictEqual(positiveSum([-1,-2,-3,-4,-5]),0);
-// assert.strictEqual(positiveSum([-1,2,3,4,-5]),9);
+assert.strictEqual(positiveSum([1,2,3,4,5]),15);
+assert.strictEqual(positiveSum([1,-2,3,4,5]),13);
+assert.strictEqual(positiveSum([]),0);
+assert.strictEqual(positiveSum([-1,-2,-3,-4,-5]),0);
+assert.strictEqual(positiveSum([-1,2,3,4,-5]),9);
