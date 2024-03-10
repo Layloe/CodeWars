d932f61f84dd669299944f6b6a6f08398e51c53e
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-03-10 10:03:00 -0700

    grow

---
+++ solution235.js
@@ -0,0 +1,27 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//? Prameters: an arr of nums
+
+//? Return: a multiplied sum of the array
+
+//? Example:
+// assert.strictEqual(grow([1, 2, 3]), 6);
+// assert.strictEqual(grow([4, 1, 1, 1, 4]), 16); 
+// assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
+
+//? Psuedocode: reduce() with * and the index set to 1 instead of zero. This way the reduce function will start the iteration index 1 allowing it to multiply through put the array
+
+const assert = require('assert')
+
+const grow = x => x.reduce((sum, curr) => sum * curr, 1)
+
+// function grow(x){
+//     return x.reduce((sum,curr) => sum * curr , 1)
+// }
+console.log(grow([4, 1, 1, 1, 4]))
+
+assert.strictEqual(grow([1, 2, 3]), 6);
+assert.strictEqual(grow([4, 1, 1, 1, 4]), 16); 
+assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
