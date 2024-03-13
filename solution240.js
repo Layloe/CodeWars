cd07bf8d090dd5f05a07fcbf2f04fa5deb8d1069
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-03-13 06:09:48 -0700

    stringToArray

--- solution240.js
+++ solution240.js
@@ -1,11 +1,29 @@
-//? Prameters:
+// Write a function to split a string and convert it into an array of words.
 
-//? Return:
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+//? Prameters: a str
+
+//? Return: an arr
 
 //? Example:
+// assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+// assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
 
-//? Psuedocode:
+//? Psuedocode: split(' ')
 
+function stringToArray(string){
+
+	return string.split(' ')
+
+}
+console.log(stringToArray("I love arrays they are my favorite"))
+
+// assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+// assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
 //         ( •_• )
 // 　　＿ノ ヽ ノ＼ __
 // 　 /　`/ ⌒Ｙ⌒
