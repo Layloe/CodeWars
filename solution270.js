c2b86a4c9d3d40532a4f1ed957b408c39691c945
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-04-10 06:47:03 -0700

    check

--- solution270.js
+++ solution270.js
@@ -1,10 +1,36 @@
-//? Prameters:
+// *** No Loops Allowed ***
 
-//? Return:
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+//? Prameters: two prams a is: array can contain numbers or strings and x is: a str or a num
+
+//? Return: a boolean depending if item is included
 
 //? Example:
+// assert.strictEqual(check([66, 101], 66), true);
+// assert.strictEqual(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+// assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+// assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
 
-//? Psuedocode:
+//? Psuedocode: use includes()
+
+const assert = require('assert')
+
+const check = (a,x) => a.includes(x)
+
+// function check(a,x){
+//     return a.includes(x)
+// };
+console.log(check([66, 101], 66))
+
+assert.strictEqual(check([66, 101], 66), true);
+assert.strictEqual(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
 
 //         ( •_• )
 // 　　＿ノ ヽ ノ＼ __
