81d17c1e2820fa2562b993625997483a369a32ab
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-04-20 09:50:43 -0700

    sorter

---
+++ solution275.js
@@ -0,0 +1,26 @@
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
+
+//? Prameters: two params both arrays with numbers
+
+//? Return: concat both arrays and remove dups
+
+//? Example:
+// assert.deepEqual(mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
+// assert.deepEqual(mergeArrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8]);
+
+//? Psuedocode: use spread operator on both params
+//? [...new Set] to remove dups
+//? sort()
+
+const assert = require('assert')
+
+function mergeArrays(a, b) {  
+    const concat = [...a, ...b].sort((a, b) => a - b)
+    return [ ...new Set(concat)]
+    // , ...new Set
+    
+  }
+console.log(mergeArrays([1, 3, 5], [2, 4, 6]))
+
+  assert.deepEqual(mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
+  assert.deepEqual(mergeArrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8]);
---
+++ solution277.js
@@ -0,0 +1,34 @@
+// HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+// The sorting should NOT be case sensitive
+
+//? Prameters: an array of strings
+
+//? Return: a array sorted asc
+
+//? Example:
+//   assert.deepEqual(sorter(['Algebra', 'history', 'Geometry', 'english']),
+//   ['Algebra', 'english', 'Geometry', 'history']);
+
+//? Psuedocode: declare var
+//? sort()
+//? work on edge cases
+
+// const assert = require('assert')
+
+function sorter(textbooks) {
+  const schoolBooks = textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1)  
+
+    return schoolBooks
+  }
+  console.log(sorter(['Algebra', 'history', 'Geometry', 'english']))
+
+
+  // assert.deepEqual(sorter(['Algebra', 'History', 'Geometry', 'English']),
+  // ['Algebra', 'English', 'Geometry', 'History']);
+
+  // assert.deepEqual(sorter(['Algebra', 'history', 'Geometry', 'english']),
+  // ['Algebra', 'english', 'Geometry', 'history']);
+
+  // assert.deepEqual(sorter(['Alg#bra', '$istory', 'Geom^try', '**english']),
+  // ['$istory', '**english', 'Alg#bra', 'Geom^try']);
