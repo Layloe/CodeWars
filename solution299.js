6a0b0fb25710bd711595662fc112dcc4c53fb8f0
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-05-21 06:37:49 -0700

    filterStringDone

--- solution299.js
+++ solution299.js
@@ -19,14 +19,17 @@
 //? Pseudocode: var split()
 // map
 // Number
-// conditionals
+// conditionals edge case
+// Number
+
+const assert = require('assert')
 
 var filterString = function(value) { // ran out of time return on str to num
-    const strSplit = value.split('').filter((item, index) => Number(item)  )
-    return strSplit
+    const strSplit = value.split('').filter((item, index) => Number(item) == 0 ? item : Number(item) ).join('')
+    return Number(strSplit)
   }
-  console.log(filterString("a1b2c3"))
+  console.log(filterString("q6s2ng8k805e02g4a8r9q8pa4"))
 
-//   assert.strictEqual(filterString("123"), 123, `For the input "123"`);
-//   assert.strictEqual(filterString("a1b2c3"), 123, `For the input "a1b2c3"`);
-//   assert.strictEqual(filterString("aa1bb2cc3dd"), 123, `For the input "aa1bb2cc3dd"`);
+  assert.strictEqual(filterString("123"), 123, `For the input "123"`);
+  assert.strictEqual(filterString("a1b2c3"), 123, `For the input "a1b2c3"`);
+  assert.strictEqual(filterString("aa1bb2cc3dd"), 123, `For the input "aa1bb2cc3dd"`);
