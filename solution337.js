22c3a08567ae709d08c3518ca2535084441f8957
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-07-13 06:39:36 -0700

    class debug

---
+++ solution337.js
@@ -0,0 +1,36 @@
+// The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null, but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+
+//? Parameters: total of two strings on with each param: first and last names
+
+//? Return: a working constructor 
+
+//? Example:
+//   Test.assertEquals(new Dinglemouse("Clint", "Eastwood").getFullName(), "Clint Eastwood")
+
+//? Pseudocode: debug: create vars in constructor with: this. Then fix the function concat
+
+const assert = require('assert')
+
+class Dinglemouse {
+
+    constructor( firstName, lastName ){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+    
+    getFullName(){
+      return this.firstName+" "+this.lastName
+    }
+    
+  }
+console.log(new Dinglemouse("Clint", "Eastwood").getFullName())
+
+assert.strictEqual(new Dinglemouse("Clint", "Eastwood").getFullName(), "Clint Eastwood")
