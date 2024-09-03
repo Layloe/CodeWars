ad05f60e700a903358a5792107c6e33ec6750b83
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-09-03 06:54:37 -0700

    liter

---
+++ solution349.js
@@ -0,0 +1,11 @@
+function litres(time) {
+    if (time <= 1) {
+        return 0
+    }
+    return  parseInt(time * .5)
+  }
+  console.log(litres(11.8))
+
+  //Input is going to be a number that is never empty
+  //Output is going to be a number will be multiplied by 0.5 and rounded down
+  
