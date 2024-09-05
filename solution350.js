c66b82011731cb7bac7e79f32290287f2e823245
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-09-05 07:01:44 -0700

    Math method ciel

--- solution350.js
+++ solution350.js
@@ -1,16 +1,22 @@
-//? Parameters: 
 
-//? Return: 
+function century(year) {
+    if(year <= 100) {
+        return 1
+    }
 
-//? Example:
+    return  Math.ceil(year / 100)
 
-//? Pseudocode: 
+  }
+  console.log(century(1705))
 
+//input is going to be a number, that is never empty
 
-//         ( •_• )
-// 　　＿ノ ヽ ノ＼ __
-// 　 /　`/ ⌒Ｙ⌒
-// 　|　ﾉ⌒＼ ￣￣ヽ　 ノ
-// 　ヽ＿＿＿＞､＿＿_／
-// 　　　 ｜( 王 ﾉ〈
-// 　　　 /ﾐ`ー―彡\
+//if the number is equal to or less than 100 return a 1, if the number is over 101 round up and divide by 100. Include the year as a four digit number of the century
+
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+// 2742 --> 28
+
+//if the number is equal to or less than 100 return a 1, else if the number is over 101 return 4 digit number that is the century.
