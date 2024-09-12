1a9155ceee98876a9ecb80efc71aee5b48e89b12
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-09-12 07:01:28 -0700

    sorting string char localeCompare

---
+++ solution355.js
@@ -0,0 +1,14 @@
+
+function last(x){
+    const splitStr = x.split(' ').sort((a,b) => a.slice(-1).localeCompare(b.slice(-1)))
+    return splitStr
+}
+console.log(last('man i need a taxi up to ubud'))
+
+//input is going to be a string of words and is never empty
+
+//output is going to be an split array of the words in the string alphabetically sorted by the last letter 
+
+//i.e ('man i need a taxi up to ubud') => ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+
+//split string then sort by last index 
