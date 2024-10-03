a15b5e05176d2d34d09fd09948b768de341b4bfc
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-10-03 06:09:59 -0700

    summation

---
+++ solution359.js
@@ -0,0 +1,25 @@
+var summation = function (num) {
+    let sum = 0
+    for (let i = 1; i <= num; i++) {
+        sum += i
+    }
+    return sum        
+  }
+//   console.log(summation(8))
+
+//Input is going to be passing in a number. The number will always be a positive integer greater than 0.
+
+//Return an integer > 0 that is a sum of the integers from 1 to num
+
+//i.e.,
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+//Declare a variable to hold a counter 
+//Loop through each integer up to but not exceeding the value num
+//use a for loop starting at 1 and going up to the value 
+//inside of each loop add the current integer value to the running total 
+ 
+
+
+
