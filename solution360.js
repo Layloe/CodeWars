99f0ef7bc2f51ce678e7c17f3051b068312163b0
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-10-08 06:53:05 -0700

    practice interview aglo

---
+++ solution360.js
@@ -0,0 +1,12 @@
+//Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+
+//num -> to that num -> div 3 fiz; div 3/5 fizz buzz
+//always int; pos; no floats; int always 
+//print to console; notes 
+
+function fizzBuzz(num) {
+
+}
+fizzBuzz(5) //1, 2, fizz,4,buzz
+fizzBuzz(9) //1,2,fizz,4,buzz.fizz,7,8,fizz
+fizzBuzz(15) //1,2,fizz,4,buzz,7, 8, fizz, buzz,13,14,fizzbuzz
---
+++ solution361.js
@@ -0,0 +1,10 @@
+// Write a function that when given a URL as a string, parse out just the domain name and return as a string. For Example:
+
+//fnc -> str url -> str domain
+//url -> http/s: no funny biz
+
+function parseDomainName(url) {
+    //replace the stuff from we don't need ""
+    //split and grab the domain 
+}
+console.log("https://github.com/Layloe/hvac/blob/main/views/index.html")
