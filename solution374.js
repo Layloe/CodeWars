aca9616ab162c1c7645f16e3397dbaede7b71b6e
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-11-22 06:51:43 -0800

    hash counter

--- solution374.js
+++ solution374.js
@@ -1,11 +1,18 @@
 // Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 // Return your answer as a number.
 
-function sum_mix(arr) {
-    const parseArr = arr.parseInt() 
-    return arr.reduce((currentItem, acc) currentItem )
-}
+// const sum_mix = arr => arr.reduce((curr, acc) => acc + parseInt(curr) ,0)
 
+function sum_mix(arr) {
+    let sum = 0
+    for (const curr of arr) {
+        sum += parseInt([curr])
+    }
+    return sum
+    // const parseArr = arr.map(item => parseInt(item))
+    // return parseArr.reduce((curr,acc) => acc + curr, 0)
+}
+console.log(sum_mix(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
 
 ///intput is going to be an arr of mixed strings and nums
 //output is going to be a number
