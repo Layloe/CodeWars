d191531a2050a71116fa176ad9812576c59d7aca
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-12-01 09:49:07 -0800

    templete literals with condtionals

--- solution382.js
+++ solution382.js
@@ -5,11 +5,21 @@
 // uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
 
 function uefaEuro2016(teams, scores){
-    // your code...
+    // const findWinSort = scores.map((item, index) => item )
+    if (scores[0] > scores[1]) {
+      return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+    }
+    if (scores[0] < scores[1]) {
+      return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+    }
+    if (scores[0] == scores[1]) {
+      return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+    }
+
   }
-  console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0]))
+  console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1]))
 
 //input will have two parameters one param is going to be an arr with two strings, next param will be an arr of two nums
 //output will be a string using templete literals and inserting team that won
 // uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
-// compare the scores arr using conditionals if 
+// compare the scores arr using conditionals to return one of 3 strings. Use index to separate teams.
