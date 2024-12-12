79b09c6508cee28a17a830124a82919f7d5c152d
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-12-12 07:14:48 -0800

    forgot test cases

--- solution393.js
+++ solution393.js
@@ -30,3 +30,9 @@ function doubleChar(str) {
   //output will double each char in the input string
   //("abcd"), "aabbccdd")
   //I will first split str to arr then use map with the method repeat then finally join
+  assert.strictEqual(doubleChar("abcd"), "aabbccdd");
+  assert.strictEqual(doubleChar("Adidas"), "AAddiiddaass");
+  assert.strictEqual(doubleChar("1337"), "11333377");
+  assert.strictEqual(doubleChar("illuminati"), "iilllluummiinnaattii");
+  assert.strictEqual(doubleChar("123456"), "112233445566");
+  assert.strictEqual(doubleChar("%^&*("), "%%^^&&**((");
