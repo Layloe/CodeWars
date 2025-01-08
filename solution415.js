ba1069657353f2edf607e2bdbeaa92c3cdbd897d
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-01-08 07:07:00 -0800

    yesterdays challenge revisit

--- solution415.js
+++ solution415.js
@@ -1,5 +1,5 @@
 // Write a function that takes an integer num (num >= 0) and inserts dashes ('-') between each two odd digits in num.
-
+const assert = require('assert')
 // Examples
 // 454793 ---> "4547-9-3"
 //      0 ---> "0"
@@ -7,16 +7,24 @@
 // 13579  ---> "1-3-5-7-9"
 //  86420 ---> "86420"
 function insertDash(num) {
-    const splitArr = String(num).split('').map((item,index )=> {
-        if (item % 2 !== 0 && [index + 1] % 2 !== 0 ) {
-            return '-' + item + '-' 
-        } else {
-            return item 
+    const toStr = String(num).split('') 
+    let dashArr = []
+    for (const curr of toStr) {
+        if (curr % 2 == 0 && [curr + 1] % 2 == 0) {
+           return dashArr += dashArr[curr] + '-'
         }
-    })
-    return splitArr.join('')
+    }
+    // const splitArr = String(num).split('').map((item, index, arr )=> {
+    //     if (item + 1 % 2 !== 0 && item % 2 !== 0 && item - 1 % 2 !== 0) {
+    //         return item + '-' 
+            
+    //     } else {
+    //         return item 
+    //     }
+    // })
+    // return splitArr
  }
-console.log(insertDash(13579)) 
+console.log(insertDash(4547939)) 
 //input is going to be a number
 //output will be a num that will have dashes ('-') between each two odd digits in num.
     // assert.strictEqual(insertDash(454793),'4547-9-3');
