5e30d433e3eeb60867b2a992eb05e4d31622db97
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-01-30 06:03:32 -0800

    Freaking typo lol

--- solution423.js
+++ solution423.js
@@ -3,15 +3,15 @@
 // Disaster!! The boat has caught fire!!
 
 // You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
-
+const assert = require('assert')
 // Go to work!
 function fireFight(s){
-  return s.split(' ').map(item => item == 'fire' ? item.replaceAll('fire', '~~') : item)
+  return s.split(' ').map(item => item.replace('Fire', '~~')).join(' ')
 }
 console.log(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"))
 // input is going to be a string of random beat words and fire
 // output will be the same string but with the word fire replaced by ~~
-    // assert.strictEqual(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"), "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast");
-    // assert.strictEqual(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
-    // assert.strictEqual(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");
+    assert.strictEqual(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"), "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast");
+    assert.strictEqual(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
+    assert.strictEqual(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");
 // split them map includes replace all fire with ~~
