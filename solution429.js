7d16d9fefd0f2e9442aff7a45248a8dac2f9c2f4
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-02-20 06:52:17 -0800

    nthChar

--- solution429.js
+++ solution429.js
@@ -29,12 +29,14 @@ function findAdmin(list, lang) {
     
     return filterAdmin 
   }
-  console.log(findAdmin( [
+  console.log(findAdmin(
+  [
     { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
     { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
     { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
     { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
-  ]))
+  ]
+))
   // input will be an array of objects 
   // return the objects that contain both; language: 'JavaScript', githubAdmin: 'yes'
 //   var list1 = [
---
+++ solution430.js
@@ -0,0 +1,23 @@
+// Complete the function that takes an array of words.
+
+// You must concatenate the nth letter from each word to construct a new word which should be returned as a string, where n is the position of the word in the list.
+
+// For example:
+
+// ["yoda", "best", "has"]  -->  "yes"
+//   ^        ^        ^
+//   n=0     n=1     n=2
+// Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
+const assert = require('assert')
+function nthChar(words){
+    let concatOfLetters = []
+    // const concatOfWords = words.reduce((sum, curr, index) =>  )]
+      
+    return words.map((item, index) => item[index]).join('')
+ }       
+   console.log(nthChar(['yoda', 'best', 'has']))
+// input will be an array of strings of random words
+// output will be a string/word of a concat of one char from each word
+    assert.strictEqual(nthChar([]),'');
+    assert.strictEqual(nthChar(['yoda', 'best', 'has']), 'yes'); 
+// declare a var that will hold logic then use map and figure how to make a counter. Hit my time limit and had to look at the answers and apparently map => item[index] counts the index as well. The more you know :)
