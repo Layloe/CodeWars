478b8c378acd69436a60fe8e1bcfaf2d3794fbd1
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-02-27 07:23:48 -0800

    numObjs

--- solution437.js
+++ solution437.js
@@ -6,17 +6,20 @@
 
 // Example
 
-// modifyMultiply ("This is a string", 3, 5) 
+// modifyMultiply ("This is a string", 3, 5)
+const assert = require('assert') 
+
 function modifyMultiply (str,loc,num) {
-    const findWord = str.split(' ').find((item, index) => item.indexOf(loc))
+    const findWord = `${(str.split(' ')[loc] + '-').repeat(num)}`.slice(-1) 
+    
     return findWord
     } 
     console.log(modifyMultiply("This is a string", 3 ,5))
     // input will be three parameters one is as string of words an the other two numbers
     // output will be a string word that is the index and is repeated by the last parameter.
-//   Test.assertEquals(modifyMultiply("This is a string",3 ,5), "string-string-string-string-string", "The string is incorrect");
-//   Test.assertEquals(modifyMultiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.",8 ,10), "that-that-that-that-that-that-that-that-that-that");
-//   Test.assertEquals(modifyMultiply("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence",1 ,1), "means");
-//   Test.assertEquals(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.",6 ,8), "ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance");
-//   Test.assertEquals(modifyMultiply("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.",2 ,5), "around-around-around-around-around");
+  assert.assertEquals(modifyMultiply("This is a string",3 ,5), "string-string-string-string-string", "The string is incorrect");
+  assert.assertEquals(modifyMultiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.",8 ,10), "that-that-that-that-that-that-that-that-that-that");
+  assert.assertEquals(modifyMultiply("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence",1 ,1), "means");
+  assert.assertEquals(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.",6 ,8), "ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance");
+  assert.assertEquals(modifyMultiply("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.",2 ,5), "around-around-around-around-around");
     //split the string then map to access index and then use repeat
