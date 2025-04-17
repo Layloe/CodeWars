b64e660d19d4dcdcada5f83f0722935294950d8b
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-04-17 09:22:25 -0700

    swap vowels to upper case

---
+++ solution482.js
@@ -0,0 +1,41 @@
+// When provided with a String, capitalize all vowels
+
+// For example:
+
+// Input : "Hello World!"
+
+// Output : "HEllO WOrld!"
+
+// Note: Y is not a vowel in this kata.
+const assert = require('assert')
+function swap (string) {
+    const vowels = 'aeiou'
+    const vowelsToUpperCase = string.split('').map(item => {
+        if (string == '') {
+            return ''
+        }
+        if (vowels.includes(item)) {
+            return item.toUpperCase()
+        } 
+        if (!vowels.includes(item)) {
+            return item
+        }
+    })
+    return vowelsToUpperCase
+  }
+  console.log(swap("HelloWorld!"))
+
+  // Input will be a string if letters or words
+  // Output is the original string with vowels capitalized
+//         assert.strictEqual("", "");
+// 		assert.strictEqual("   @@@", "   @@@");
+// 		assert.strictEqual("HelloWorld!", "HEllOWOrld!");
+// 		assert.strictEqual("Sunday", "SUndAy");
+// 		assert.strictEqual("Codewars", "COdEwArs");
+// 		assert.strictEqual("Monday", "MOndAy");
+// 		assert.strictEqual("Friday", "FrIdAy");
+// 		assert.strictEqual("abracadabra", "AbrAcAdAbrA");
+// 		assert.strictEqual("AbrAcAdAbrA", "AbrAcAdAbrA");
+// 		assert.strictEqual("ABRACADABRA", "ABRACADABRA");
+// 		assert.strictEqual("aBRaCaDaBRa", "ABRACADABRA");
+//   // Create a variable that will hold vowels then split then map use conditionals to see if vowels are included in argument if do toupper then else item. 
