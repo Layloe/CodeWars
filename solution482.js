752f44f4b3cdccc575fbeca576b6a51c13af298c
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-04-18 06:18:35 -0700

    recursion

--- solution482.js
+++ solution482.js
@@ -8,20 +8,20 @@
 
 // Note: Y is not a vowel in this kata.
 const assert = require('assert')
+
 function swap (string) {
     const vowels = 'aeiou'
+    // const capVowel = string.split('').map(item => vowels.includes(item) ? item.toUpperCase() : item).join('')
     const vowelsToUpperCase = string.split('').map(item => {
-        if (string == '') {
-            return ''
-        }
         if (vowels.includes(item)) {
             return item.toUpperCase()
         } 
         if (!vowels.includes(item)) {
             return item
         }
-    })
-    return vowelsToUpperCase
+        })
+
+    return vowelsToUpperCase.join('')   
   }
   console.log(swap("HelloWorld!"))
 
@@ -38,4 +38,6 @@ function swap (string) {
 // 		assert.strictEqual("AbrAcAdAbrA", "AbrAcAdAbrA");
 // 		assert.strictEqual("ABRACADABRA", "ABRACADABRA");
 // 		assert.strictEqual("aBRaCaDaBRa", "ABRACADABRA");
-//   // Create a variable that will hold vowels then split then map use conditionals to see if vowels are included in argument if do toupper then else item. 
+  // Create a variable that will hold vowels then split then map use conditionals to see if vowels are included in argument if do toupper then else item. 
+
+
