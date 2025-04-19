2972b5cf552167bfcf7f1a825b8e5ba483395467
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-04-19 08:35:41 -0700

    edge cases for isSameLanguage

--- solution483.js
+++ solution483.js
@@ -1,9 +1,36 @@
-import OpenAI from "openai";
-const client = new OpenAI();
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
 
-const response = await client.responses.create({
-    model: "gpt-4.1",
-    input: "Write a one-sentence bedtime story about a unicorn."
-});
+// Your task is to return either:
 
-console.log(response.output_text);
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+// your function should return true.
+
+// Notes:
+
+// The strings representing a given language will always be formatted in the same way (e.g. 'JavaScript' will always be formatted will upper-case 'J' and 'S'
+// The input array will always be valid and formatted as in the example above.
+
+function isSameLanguage(list) {
+    const langJS = list.filter(item => item.language !== list[0].language)
+
+    return langJS.length === 0
+}
+console.log(isSameLanguage([
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+  ]))
+//   [{"firstName":"Mark","lastName":"G.","country":"Scotland","continent":"Europe","age":22,"language":"JavaScript"},{"firstName":"Victoria","lastName":"T.","country":"Puerto Rico","continent":"Americas","age":30,"language":"Clojure"},{"firstName":"Emma","lastName":"B.","country":"Norway","continent":"Europe","age":19,"language":"Clojure"}]
+//    [{"firstName":"Kseniya","lastName":"T.","country":"Belarus","continent":"Europe","age":29,"language":"Ruby"},{"firstName":"Amar","lastName":"V.","country":"Bosnia and Herzegovina","continent":"Europe","age":32,"language":"Ruby"}]
+  // Input is an array of objects 
+  // Output is going to be a boolean
+  // Example is above 
+  // Create a variable that will hold filter then use if else to check if item.language is equal to list[0].length then use length to compare
