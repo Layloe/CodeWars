9a10653b3afaa5534d15b3f0f5978233849f4ed1
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-05-02 07:17:09 -0700

    move ten testing

---
+++ solution497.js
@@ -0,0 +1,26 @@
+// Move every letter in the provided string forward 10 letters through the alphabet.
+
+// If it goes past 'z', start again at 'a'.
+
+// Input will be a string with length > 0.
+
+ function moveTen(s) {
+    const lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    const findLetters = s.split('').map((item, index) => {
+        console.log(lowercaseAlphabet[index % lowercaseAlphabet.length])
+    //    return lowercaseAlphabet[index % lowercaseAlphabet.length]
+        if (lowercaseAlphabet.includes(item)) {
+            return lowercaseAlphabet.indexOf(item)
+        }
+        
+    //    return lowercaseAlphabet[index]
+    })
+    return findLetters
+ }
+ console.log(moveTen("testcase"))
+ // Input is a string with length > 0.
+ // Output is a string with index moved 10 places
+    // assert.strictEqual(moveTen("testcase"), "docdmkco");
+    // assert.strictEqual(moveTen("codewars"), "mynogkbc");
+    // assert.strictEqual(moveTen("exampletesthere"), "ohkwzvodocdrobo");
+// Find alphabet string and create a variable to hold string then split map to array and use index to match alphabet google how to keep looping thru string one it's reached it's end. 
