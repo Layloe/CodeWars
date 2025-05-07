093e19df8e3997fa0f3ef6768184d00289adc7d1
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-05-07 07:18:00 -0700

    get average age

--- solution503.js
+++ solution503.js
@@ -4,10 +4,11 @@
 // "79797"    --> "777"
 
 function sevenAte9(str) {
-  return str.split('9').join('')
+
+  return str.split('797').join('77')
 }
 console.log(sevenAte9("165561786121789797"))
 // Input is a string of numbers.
 // Output is the string but with '9' removed.
 // Example is above
-// Split with '9' then join
+// Split with '797' then join with 77.
