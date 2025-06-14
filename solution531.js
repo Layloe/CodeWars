a2ed0ee5a4b78cce8c5f60e1047fcfae58f0076c
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-06-14 10:51:15 -0700

    vowel index

---
+++ solution531.js
@@ -0,0 +1,33 @@
+// Check if it is a vowel(a, e, i, o, u,) on the n position in a string (the first argument). Don't forget about uppercase.
+
+// A few cases:
+
+// {
+// checkVowel('cat', 1)  ->   true // 'a' is a vowel
+// checkVowel('cat', 0)  ->   false // 'c' is not a vowel
+// checkVowel('cat', 4)  ->   false // this position doesn't exist
+// }
+function checkVowel(string, position) {
+  const vowels = 'AEIOUaeiou'
+  const isVowel = string.split('').map((item, index, arr)=> {
+    console.log(vowels.includes(item))
+    console.log(arr[position] )
+    if (vowels.includes(item) && arr.indexOf(position)) {
+        return true
+    } else {
+        return false
+    }
+  })
+return isVowel
+}
+console.log(checkVowel('cat', 1))
+// Input has two parameters, one is a string the other a number.
+// Output is a boolean.
+    // assert.strictEqual(checkVowel('cat', 1), true);
+    // assert.strictEqual(checkVowel('cat', 0), false);
+    // assert.strictEqual(checkVowel('cat', 4), false);
+
+    // assert.strictEqual(checkVowel('Amanda', -2), false);
+    // assert.strictEqual(checkVowel('Amanda', 0), true);
+    // assert.strictEqual(checkVowel('Amanda', 2), true);  
+// Declare a variable that will hold vowels string then split map and use if else to return boolean.
