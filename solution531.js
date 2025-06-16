8df8c69a0773379a5d3304f0198816a2c92ee98b
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-06-16 09:00:49 -0700

    bucket of

--- solution531.js
+++ solution531.js
@@ -7,27 +7,28 @@
 // checkVowel('cat', 0)  ->   false // 'c' is not a vowel
 // checkVowel('cat', 4)  ->   false // this position doesn't exist
 // }
+const assert = require('assert')
 function checkVowel(string, position) {
-  const vowels = 'AEIOUaeiou'
-  const isVowel = string.split('').map((item, index, arr)=> {
-    console.log(vowels.includes(item))
-    console.log(arr[position] )
-    if (vowels.includes(item) && arr.indexOf(position)) {
-        return true
-    } else {
-        return false
-    }
-  })
-return isVowel
+  const vowels = 'AEIOUaeiou'.includes(string[position])
+//   const isVowel = string.map((item, index, arr)=> {
+    // console.log(vowels.includes(item))
+    // console.log(arr[position] )
+    // if (vowels.includes(item[position])) {
+    //     return true
+    // } else {
+    //     return false
+    // }
+//   })
+return vowels
 }
 console.log(checkVowel('cat', 1))
 // Input has two parameters, one is a string the other a number.
 // Output is a boolean.
-    // assert.strictEqual(checkVowel('cat', 1), true);
-    // assert.strictEqual(checkVowel('cat', 0), false);
-    // assert.strictEqual(checkVowel('cat', 4), false);
+    assert.strictEqual(checkVowel('cat', 1), true);
+    assert.strictEqual(checkVowel('cat', 0), false);
+    assert.strictEqual(checkVowel('cat', 4), false);
 
-    // assert.strictEqual(checkVowel('Amanda', -2), false);
-    // assert.strictEqual(checkVowel('Amanda', 0), true);
-    // assert.strictEqual(checkVowel('Amanda', 2), true);  
+    assert.strictEqual(checkVowel('Amanda', -2), false);
+    assert.strictEqual(checkVowel('Amanda', 0), true);
+    assert.strictEqual(checkVowel('Amanda', 2), true);  
 // Declare a variable that will hold vowels string then split map and use if else to return boolean.
---
+++ solution532.js
@@ -0,0 +1,34 @@
+// If you say any of the following words a large bucket of "water" will be dumped on you: "water", "wet", "wash" This is true for any form of those words, like "washing", "watered", etc.
+
+// If you say any of the following phrases you will be doused in "slime": "I don't know", "slime"
+
+// If you say both in one sentence, a combination of water and slime, "sludge", will be dumped on you.
+
+// Write a function, bucketOf(str), that takes a string and determines what will be dumped on your head. If you haven't said anything you shouldn't have, the bucket should be filled with "air". The words should be tested regardless of case.
+
+function bucketOf(str) {
+    const containsStr = str.toLowerCase().split(' ').filter(item =>!'!?\''.includes(item))
+    // .filter(item => {
+    //     item == 'is'
+    //     // if (["water", "wet", "wash"].includes(item)) {
+    //     //     return 'water'
+    //     // }
+    //     // if ('slime'.includes(item)) {
+    //     //     return 'slime'
+    //     // } else {
+    //     //     return 'air'
+    //     // }
+    // })
+    // console.log(containsStr)
+    return containsStr.filter(item => 'water' == item)
+//   "water", "wet", "wash"
+//   "slime"
+//   "air"
+}
+console.log(bucketOf("What is that, WATER?!?"))
+// Input is a string.
+// Output will be one of three strings.
+// bucketOf("What is that, WATER?!?") -> "water"
+// bucketOf("I don't know if I'm doing this right.") -> "slime"
+// bucketOf("You won't get me!") -> "air"
+// Split, filter and use if else and lowercase to see if contains strings. 
