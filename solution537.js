2562b9b8f8bf6d42f4edf71b31d383b51bf63fcb
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-06-21 09:08:32 -0700

    hash practice

---
+++ solution537.js
@@ -0,0 +1,27 @@
+// Valid Anagram
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+// Example 1:
+
+// Input: s = "racecar", t = "carrace"
+
+// Output: true
+// Example 2:
+
+// Input: s = "jar", t = "jam"
+
+// Output: false
+
+function isAnagram(s, t) {
+    console.log([...t].sort())
+    // let results = []
+    // for (item of s) {
+    //     if (item[t]) {
+    //         item[t]
+    //     }
+    // }
+    return [...s].sort().includes([...t].sort())
+}
+console.log(isAnagram('racecar', 'carrace'))
