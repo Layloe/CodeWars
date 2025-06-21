d8a5a192d270e10e73aa8c25dd501f3cc6bb983c
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-06-21 08:55:26 -0700

    hashmap practice

---
+++ solution536.js
@@ -0,0 +1,32 @@
+// Contains Duplicate
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1, 2, 3, 4]
+
+// Output: false
+
+function dup(arr) {
+    // const results = []
+    // for (item of arr) {
+    //     if (results[item]) {
+    //         results[item] 
+    //     } else {
+    //         results[item] 
+    //     }
+    // }
+    // return results
+    return new Set(arr).length < arr.length
+}
+console.log(dup([1, 2, 3, 3]))
+// Input is an array of numbers.
+// Output is a boolean.
+// Example above.
+// Map then use if else if item contains index.
