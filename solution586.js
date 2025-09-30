f0e1e1bab64649e6a5c44a969a1c7044b445e8eb
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-09-30 07:23:03 -0700

    sorting and indexing arr

---
+++ solution586.js
@@ -0,0 +1,28 @@
+// Your task is very simple. Just write a function that takes an input string of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+
+// Examples (input -> output)
+// "kata" -> false ('a' comes after 'k')
+// "ant" -> true (all characters are in alphabetical order)
+// Good luck :)
+
+function alphabetic(s) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const checkStr = s.split('').sort().map((item, index)=> {
+    console.log(alphabet.index + 1)
+    if (alphabet.indexOf(item) < alphabet.indexOf((item + 1)) ) {
+        return true
+    }
+  })
+
+  return checkStr 
+}
+console.log(alphabetic('asd'))
+// Input is a string of lowercase letters.
+// Output is a boolean that depends on whether or not the words are in alphabetical order.
+    // assert.strictEqual("asd", false);
+    // assert.strictEqual("codewars", false)
+    // assert.strictEqual("door", true);
+    // assert.strictEqual("cell", true);
+    // assert.strictEqual("z", true);
+    // assert.strictEqual("", true);  
+// Split and sort and if else to see if index is lower then the following index.
