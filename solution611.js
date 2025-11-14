b2924dcfe90d578e92b79744a44d45f9ca1cd751
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-11-14 07:23:35 -0800

    timed hashmap challenge

---
+++ solution611.js
@@ -0,0 +1,31 @@
+// You will be given an array of objects (associative arrays in PHP, table in COBOL, dictionaries in Python) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object (associative array in PHP, table in COBOL, dictionary in Python) which includes the count of each coding language represented at the meetup.
+
+// For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+];
+// your function should return the following object (associative array in PHP, table in COBOL):
+
+// { C: 2, JavaScript: 1, Ruby: 1 }
+function countLanguages(list) {
+  let results = {}
+  for (lang of list) {
+    if (results[lang]) {
+        results[lang]++
+    } else {
+        results[lang] = 1
+    }
+  }
+  return results
+}
+console.log(countLanguages(list1))
+// Input is an array of objects.
+// Output is and object containing lang and count.
+// Example above.
+// Hashmap with conditional.
