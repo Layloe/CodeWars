ff9cd8ceac31d51430c0d796bf4a834bacfa6733
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2026-01-05 07:26:05 -0800

    executing steps in order

--- solution623.js
+++ solution623.js
@@ -10,20 +10,25 @@
 // { 1, 1, 11, 2, 3 } => 6
 // Input validation
 // If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+const assert = require('assert')
 
 function sumArray(array) {
-    const getSum = array.sort((a,b) => a - b).slice(0, -1).reduce((acc, curr) => curr + acc, 1)
+    if (array == [] ||array === null || array.length < 2) {
+        return 0
+    }
+    const getSum = array.sort((a,b) => a - b).slice(1, -1).reduce((acc, curr) => curr + acc, 0)
+
     return getSum
 }
 console.log(sumArray([ 6, 2, 1, 8, 10 ]))
 // Input is a an array of numbers.
 // Output is the sum of the numbers minus highest and lowest number.
-//   assert.strictEqual( sumArray(null)                     , 0 );
-//   assert.strictEqual( sumArray([ ])                      , 0 );
-//   assert.strictEqual( sumArray([ 3 ])                    , 0 );
-//   assert.strictEqual( sumArray([ 3, 5 ])                 , 0 );
-//   assert.strictEqual( sumArray([ 6, 2, 1, 8, 10 ])       , 16 );
-//   assert.strictEqual( sumArray([ 0, 1, 6, 10, 10 ])      , 17 );
-//   assert.strictEqual( sumArray([ -6, -20, -1, -10, -12 ]), -28 );
-//   assert.strictEqual( sumArray([ -6, 20, -1, 10, -12 ])  , 3 );
+  assert.strictEqual( sumArray(null)                     , 0 );
+  assert.strictEqual( sumArray([ ])                      , 0 );
+  assert.strictEqual( sumArray([ 3 ])                    , 0 );
+  assert.strictEqual( sumArray([ 3, 5 ])                 , 0 );
+  assert.strictEqual( sumArray([ 6, 2, 1, 8, 10 ])       , 16 );
+  assert.strictEqual( sumArray([ 0, 1, 6, 10, 10 ])      , 17 );
+  assert.strictEqual( sumArray([ -6, -20, -1, -10, -12 ]), -28 );
+  assert.strictEqual( sumArray([ -6, 20, -1, 10, -12 ])  , 3 );
 // Create a variable that will sort, slice last item, and reduce to a sum.
