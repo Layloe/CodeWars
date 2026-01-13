8a0e0e694b7d8c25e2c445d37f3de45ff9f81775
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2026-01-13 10:41:28 -0800

    suming time stamps

--- solution626.js
+++ solution626.js
@@ -6,6 +6,18 @@
 // Your Task:
 // You will get an array as input with time durations as string in the following format: HH:MM:SS. Each duration represents the time taken by Santa to deliver a present. Determine whether he can do it in 24 hours or not. In case the time required to deliver all of the presents is exactly 24 hours, Santa can complete the delivery ;-) .
 function determineTime (durations) {
-  //have fun with coding ^.^
+  const getSum = durations.map(time => time.split(':').join('')).reduce((acc, curr) => parseInt(curr) + acc,  0)
+  // 
+
+  return toSeconds(getSum)
 }
 console.log(determineTime(["00:30:00", "02:30:00", "00:15:00"]))
+// Input is an array if time durations in hours/minutes/seconds.
+// Output is a boolean if time is met.
+  // doTest(["00:30:00", "02:30:00", "00:15:00"], true);
+	// doTest([], true);
+	// doTest(["04:30:00", "02:00:00", "01:30:00", "16:00:00"], true);
+	// doTest(["12:00:00", "12:00:00"], true);
+	// doTest(["12:00:00", "12:00:01"], false);
+	// doTest(["06:00:00","12:00:00","06:30:00"], false);
+// Going to give reduce a try. Make a variable that will split ':' the array then join and finally reduce the array then if else if greater that 24
