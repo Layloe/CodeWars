4a0ccbb03cfc6049b066ae8d2d51dd29f571d6e3
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2026-01-16 07:22:09 -0800

    review solved

--- solution626.js
+++ solution626.js
@@ -6,10 +6,10 @@
 // Your Task:
 // You will get an array as input with time durations as string in the following format: HH:MM:SS. Each duration represents the time taken by Santa to deliver a present. Determine whether he can do it in 24 hours or not. In case the time required to deliver all of the presents is exactly 24 hours, Santa can complete the delivery ;-) .
 function determineTime (durations) {
-  const getSum = durations.map(time => time.split(':').join('')).reduce((acc, curr) => parseInt(curr) + acc,  0)
-  // 
+  const getSum = durations.map(time => time.split(':').join('')).reduce((acc, curr) => parseInt(curr) + acc,  0) 
+  
 
-  return toSeconds(getSum)
+  return getSum 
 }
 console.log(determineTime(["00:30:00", "02:30:00", "00:15:00"]))
 // Input is an array if time durations in hours/minutes/seconds.
@@ -20,4 +20,4 @@ console.log(determineTime(["00:30:00", "02:30:00", "00:15:00"]))
 	// doTest(["12:00:00", "12:00:00"], true);
 	// doTest(["12:00:00", "12:00:01"], false);
 	// doTest(["06:00:00","12:00:00","06:30:00"], false);
-// Going to give reduce a try. Make a variable that will split ':' the array then join and finally reduce the array then if else if greater that 24
+// Going to give reduce a try. Make a variable that will split ':' the array then join and finally reduce the array then if else if greater that 24.//! return to turning time into seconds
