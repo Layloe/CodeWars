93e9ed50e0632eee576fcc20b05dd08170440bce
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2026-01-26 07:24:58 -0800

    puzzle question

---
+++ solution627.js
@@ -0,0 +1,14 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+    return 'weird'.length
+}
+console.log(unusualFive())
+// Input is a empty.
+// Output is always the number five and can't use any of the following characters: 0123456789*+-/.
+// Puzzle question.
+// Use a string that contains five letters.
