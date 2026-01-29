471371fe6ffba44832594af449a287ae4f9794a1
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2026-01-29 07:45:30 -0800

    string methods and compare

---
+++ solution630.js
@@ -0,0 +1,22 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+const assert = require('assert')
+
+function isPalindrome(x) {
+    return x.toLowerCase().split('').reverse().join('') === x.toLowerCase()
+}
+console.log(isPalindrome('racecar'))
+
+// Input is a string.
+// Output is a boolean.
+  assert.strictEqual(isPalindrome("a"), true);
+  assert.strictEqual(isPalindrome("aba"), true);
+  assert.strictEqual(isPalindrome("Abba"), true);
+  assert.strictEqual(isPalindrome("hello"), false);
+  assert.strictEqual(isPalindrome("Bob"), true);
+  assert.strictEqual(isPalindrome("Madam"), true);
+  assert.strictEqual(isPalindrome("AbBa"), true);
+  assert.strictEqual(isPalindrome(""), true);
+// Split reverse and join. Then compare.
