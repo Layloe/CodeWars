81af414dfb520f1b4a9a24e182b65254d53b810f
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2026-02-12 07:27:21 -0800

    ran out of time

---
+++ solution635.js
@@ -0,0 +1,34 @@
+// Kata Task
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+function humanYearsCatYearsDogYears(humanYears) {
+    if (humanYears >= 1 + 15) {
+        
+    }
+
+
+}
+console.log(humanYearsCatYearsDogYears([10,56,64]))
+// Input is a an array of three integers.
+// Output is number sum.
+    // Test.assertSimilar(humanYearsCatYearsDogYears(1), [1,15,15]);
+    // Test.assertSimilar(humanYearsCatYearsDogYears(2), [2,24,24]);
+    // Test.assertSimilar(humanYearsCatYearsDogYears(10), [10,56,64]);
+// Use conditional to age is to 1
